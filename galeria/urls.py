c33fbb15827efad478511bdfcc4617d036cfe338
Andrieli <andrieliluci@gmail.com>
2023-02-05 19:41:44 -0300

    atualizando links através da adição de um name

--- galeria/urls.py
+++ galeria/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from galeria.views import index, imagem
 
 urlpatterns = [
-    path('', index),
-    path('imagem/', imagem)
+    path('', index, name='index'),
+    path('imagem/', imagem, name='imagem')
 ]
