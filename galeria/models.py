e3847cf59e7e3ddfb09634e72da51805ffdeb9e2
Andrieli <andrieliluci@gmail.com>
2023-02-09 21:49:46 -0300

    criando models

--- galeria/models.py
+++ galeria/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Fotografia(models.Model):
+    # colunas da tabela Fotografia
+    nome = models.CharField(max_length=100, null=False, blank=False)
+    legenda = models.CharField(max_length=150, null=False, blank=False)
+    descricao = models.TextField(null=False, blank=False)
+    foto = models.CharField(max_length=100, null=False, blank=False)
+
+    # boa prática
+    def __str__(self):
+        return f'Fotografia [nome={self.nome}]'
