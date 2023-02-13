fcb9ea3f69c0f7882ce7f66c999131158ff1ba9d
Andrieli <andrieliluci@gmail.com>
2023-02-12 21:23:21 -0300

    melhorando a visualização dos itens no admin, colocando data e ordenação

--- galeria/models.py
+++ galeria/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from datetime import datetime
+
 class Fotografia(models.Model):
 
     OPCOES_DE_CATEGORIA = [
@@ -15,6 +17,8 @@ class Fotografia(models.Model):
     categoria = models.CharField(max_length=100, choices=OPCOES_DE_CATEGORIA, default='')
     descricao = models.TextField(null=False, blank=False)
     foto = models.CharField(max_length=100, null=False, blank=False)
+    publicada = models.BooleanField(default=False)
+    data_fotografia = models.DateTimeField(default=datetime.now, blank=False)
 
     # boa prática
     def __str__(self):
