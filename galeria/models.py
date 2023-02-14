38c7106d364bbde39819b1547484705640113dbb
Andrieli <andrieliluci@gmail.com>
2023-02-14 19:45:06 -0300

    manipulando imagens através do admin

--- galeria/models.py
+++ galeria/models.py
@@ -16,10 +16,10 @@ class Fotografia(models.Model):
     legenda = models.CharField(max_length=150, null=False, blank=False)
     categoria = models.CharField(max_length=100, choices=OPCOES_DE_CATEGORIA, default='')
     descricao = models.TextField(null=False, blank=False)
-    foto = models.CharField(max_length=100, null=False, blank=False)
+    foto = models.ImageField(upload_to='fotos/%Y/%m/%d/', blank=True)
     publicada = models.BooleanField(default=False)
     data_fotografia = models.DateTimeField(default=datetime.now, blank=False)
 
     # boa prática
     def __str__(self):
-        return f'Fotografia [nome={self.nome}]'
+        return self.nome
