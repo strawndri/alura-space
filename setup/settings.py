ce3bf0d0c2d48e3b327dd8a8a29b1af54924ffee
Andrieli <andrieliluci@gmail.com>
2023-02-18 14:30:06 -0300

    alterando o estilo no campo de busca do footer

--- setup/settings.py
+++ setup/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = str(os.getenv('SECRET_KEY'))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = []
 
 # Application definition
 
