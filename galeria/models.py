358dd4bbb79158c320adb46be838f9e938917319
Andrieli <andrieliluci@gmail.com>
2023-02-19 20:26:39 -0300

    nova migration -> apresentar usuário que adicionou uma nova imagem à galeria

--- galeria/models.py
+++ galeria/models.py
@@ -2,6 +2,8 @@ from django.db import models
 
 from datetime import datetime
 
+from django.contrib.auth.models import User
+
 class Fotografia(models.Model):
 
     OPCOES_DE_CATEGORIA = [
@@ -19,6 +21,14 @@ class Fotografia(models.Model):
     foto = models.ImageField(upload_to='fotos/%Y/%m/%d/', blank=True)
     publicada = models.BooleanField(default=False)
     data_fotografia = models.DateTimeField(default=datetime.now, blank=False)
+
+    usuario = models.ForeignKey(
+        to = User,
+        on_delete = models.SET_NULL,
+        null = True,
+        blank = False,
+        related_name = 'user'
+    )
 
     # boa prática
     def __str__(self):
