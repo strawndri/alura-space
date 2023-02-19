001ef73a236efe7e6e53b0165ef190bd4aa01cf9
Andrieli <andrieliluci@gmail.com>
2023-02-19 07:45:09 -0300

    construção da lógica de login e cadastro utilizando autenticação

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,12 +1,62 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from usuarios.forms import LoginForms, CadastroForms
 
+from django.contrib.auth.models import User
+
+from django.contrib import auth
+
 def login(request):
-    form = LoginForms
+    form = LoginForms()
+
+    if request.method == 'POST':
+        form = LoginForms(request.POST)
+
+        if form.is_valid():
+            nome = form['nome_login'].value()
+            senha = form['senha'].value()
+
+            usuario = auth.authenticate(
+                request,
+                username=nome,
+                password=senha
+            )
+
+            if usuario is not None:
+                auth.login(request, usuario)
+                return redirect('index')
+            else:
+                return redirect('login')
+
+
     return render(request, 'usuarios/login.html', {'form': form})
 
 
 def cadastro(request):
-    form = CadastroForms
+    form = CadastroForms()
+
+    if request.method == 'POST':
+        form = CadastroForms(request.POST)
+
+        if form.is_valid():
+            # priorizar as alternativas para evitar uma quantidade desnecessária de if's
+            if form['senha_1'].value() != form['senha_2'].value(): 
+                return redirect('cadastro')
+
+            nome = form['nome_cadastro'].value()
+            email = form['email'].value()
+            senha = form['senha_1'].value()
+
+            if User.objects.filter(username='nome').exists():
+                return redirect('cadastro')
+            
+            usuario = User.objects.create_user(
+                username = nome,
+                email = email,
+                password = senha
+            )
+
+            usuario.save()
+            return redirect('login')
+
     return render(request, 'usuarios/cadastro.html', {'form': form})
